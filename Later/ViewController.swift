d51a1bf61408398960e53e14cb5e8c6dbdd0e37a
kalyssao <klysssa@icloud.com>
2020-05-22 12:07:50 +0000

    Initial Commit

---
+++ Later/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Later
+//
+//  Created by LVZ on 5/22/20.
+//  Copyright © 2020 LVZ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
